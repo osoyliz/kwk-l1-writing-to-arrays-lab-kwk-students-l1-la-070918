18fa019dcf4af89693d6136b95f6d2df9a3bc6e6
osoyliz <lizosoy18@gmail.com>
2018-07-11 16:29:30 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -13,6 +13,11 @@ def change_rainbow_colors
 end
 change_rainbow_colors
 
+rainbow_colors = [
+  "red",
+  "light_red",
+  "light_yellow"
+]
 def add_colors
     puts rainbow_colors[3] = "green"
     puts rainbow_colors[4] = "blue"
